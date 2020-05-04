20d5a4d4cabd50f2a49ee2eabd33ae04abbb1bf1
thitiph0n <thitipontk@gmail.com>
2020-05-04 17:25:50 +0700

    cors config

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 //cross origin allow
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 //accept Json body
 app.use(express.json());
