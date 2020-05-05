aace3ccc60066199298979c4ee1257a043dde3d5
thitiph0n <thitipontk@gmail.com>
2020-05-06 03:23:34 +0700

    build skeleton

--- modules/enrollments.js
+++ modules/enrollments.js
@@ -8,4 +8,96 @@ const hasRole = require("../helpers/hasRole");
 //authorize before access
 router.use(authorization);
 
+/**create enrollmentId**/
+router.post("/create", hasRole([1]), async (req, res) => {
+  try {
+    res.json({ message: "Enrollment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: { message: error.sqlMessage || error, code: error.code },
+    });
+  }
+});
+
+/**confirm that enrollment**/
+router.post("/confirm", hasRole([1]), async (req, res) => {
+  try {
+    res.json({ message: "Enrollment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: { message: error.sqlMessage || error, code: error.code },
+    });
+  }
+});
+
+/**search subject to enroll**/
+router.get("/subjects/:subjectId", hasRole([1]), async (req, res) => {
+  try {
+    res.json({ message: "Enrollment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: { message: error.sqlMessage || error, code: error.code },
+    });
+  }
+});
+
+/**get subject you enroll by enrollmentId**/
+router.get("/:enrollmentId", hasRole([1]), async (req, res) => {
+  try {
+    res.json({ message: "Enrollment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: { message: error.sqlMessage || error, code: error.code },
+    });
+  }
+});
+
+/**delete subject you enroll by subjectId and sectionId**/
+router.get(
+  "/:enrollmentId/:subjectId/:sectionId/delete",
+  hasRole([1]),
+  async (req, res) => {
+    try {
+      res.json({ message: "Enrollment" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: { message: error.sqlMessage || error, code: error.code },
+      });
+    }
+  }
+);
+
+/**reserve the seat**/
+router.post(
+  "/:enrollmentId/:subjectId/:sectionId",
+  hasRole([1]),
+  async (req, res) => {
+    try {
+      res.json({ message: "Enrollment" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: { message: error.sqlMessage || error, code: error.code },
+      });
+    }
+  }
+);
+
+/**get enrollment of this student**/
+router.get("/", hasRole([1]), async (req, res) => {
+  try {
+    res.json({ message: "Enrollment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: { message: error.sqlMessage || error, code: error.code },
+    });
+  }
+});
+
 module.exports = router;
